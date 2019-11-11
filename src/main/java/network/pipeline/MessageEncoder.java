8f7267aefdde4e2487d1009b9c105155142875c9
Pedro Fouto <pfouto10@gmail.com>
2019-11-11 16:24:56 +0000

    Added exception throw when serialized size is incorrect

--- src/main/java/network/pipeline/MessageEncoder.java
+++ src/main/java/network/pipeline/MessageEncoder.java
@@ -10,24 +10,31 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
-public class MessageEncoder extends MessageToByteEncoder<NetworkMessage>
-{
+public class MessageEncoder extends MessageToByteEncoder<NetworkMessage> {
     private static final Logger logger = LogManager.getLogger(MessageEncoder.class);
 
     private Map<Short, ISerializer> serializers;
 
-    public MessageEncoder(Map<Short, ISerializer> serializers)
-    {
+    public MessageEncoder(Map<Short, ISerializer> serializers) {
         this.serializers = serializers;
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, NetworkMessage msg, ByteBuf out)
-    {
+    protected void encode(ChannelHandlerContext ctx, NetworkMessage msg, ByteBuf out) {
+        int initialIndex = out.writerIndex();
+
         ISerializer iSerializer = serializers.get(msg.code);
-        out.writeInt(iSerializer.serializedSize(msg.payload) + 2);
+        int serializedSize = iSerializer.serializedSize(msg.payload);
+        out.writeInt(serializedSize + 2);
         out.writeShort(msg.code);
         iSerializer.serialize(msg.payload, out);
+
+        int writtenBytes = out.writerIndex() - initialIndex;
+
+        if (writtenBytes != (serializedSize + 2 + 4)) {
+            throw new RuntimeException(
+                    "Size of message " + msg.getClass() + "( " + msg + " ) is incorrect. Reported value is " + serializedSize + " but internal buffer has " + (writtenBytes - 2 - 4));
+        }
 
         assert out.writerIndex() == iSerializer.serializedSize(msg.payload) + 2 + 4;
     }
